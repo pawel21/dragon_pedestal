a86f7ed3158d3b82808fa4f070a98212d8857fd8
Paweł Gliwny <p.gliwny@gmail.com>
2018-03-31 14:26:23 +0200

    define function to comput pedestal

--- release/pedestal.py
+++ release/pedestal.py
@@ -13,19 +13,30 @@ class PedestalSimple:
         self.number_of_event_high_gain = np.zeros((self.n_channels, self.size4drs))
         self.mean_value_channel_high_gain = np.zeros((self.n_channels, self.size4drs))
 
+        self.channel_pedestal_value_low_gain = np.zeros((self.n_channels, self.size4drs))
+        self.number_of_event_low_gain = np.zeros((self.n_channels, self.size4drs))
+        self.mean_value_channel_low_gain = np.zeros((self.n_channels, self.size4drs))
+
     def fill_pedestal_event(self, event):
         for j in range(0, self.n_channels):
             for k in range(2, self.RoI - 2):
                 # for high gain
-                position = int((k + event.first_capacitor_high_gain[j]) % self.size4drs)
-                self.channel_pedestal_value_high_gain[j, position] += event.samples_high_gain[j, k]
-                self.number_of_event_high_gain[j, position] += 1
+                position_hg = int((k + event.first_capacitor_high_gain[j]) % self.size4drs)
+                self.channel_pedestal_value_high_gain[j, position_hg] += event.samples_high_gain[j, k]
+                self.number_of_event_high_gain[j, position_hg] += 1
+                # for low gain
+                position_lg = int((k + event.first_capacitor_low_gain[j]) % self.size4drs)
+                self.channel_pedestal_value_low_gain[j, position_lg] += event.samples_low_gain[j, k]
+                self.number_of_event_low_gain[j, position_lg] += 1
 
     def finalize_pedestal(self):
         with warnings.catch_warnings():
             warnings.filterwarnings('error')
             try:
-                self.mean_value_channel_high_gain = self.channel_pedestal_value_high_gain[:, :]/self.number_of_event_high_gain[:, :]
+                self.mean_value_channel_high_gain = self.channel_pedestal_value_high_gain[:, :]\
+                                                    /self.number_of_event_high_gain[:, :]
+                self.mean_value_channel_low_gain = self.channel_pedestal_value_low_gain[:,:] \
+                                                    / self.number_of_event_low_gain[:, :]
             except Warning as e:
                 print("Not enough events. Error: ", e)
 
@@ -35,5 +46,9 @@ def remove_pedestal(event, pedestal):
     size4drs = 4 * 1024
     for j in range(0, n_channels):
         for k in range(0, event.RoI):
-            position = int((k + event.first_capacitor_high_gain[j]) % size4drs)
-            event.samples_high_gain[j, k] -= pedestal.mean_value_channel_high_gain[j, position]
+            # for high gain
+            position_hg = int((k + event.first_capacitor_high_gain[j]) % size4drs)
+            event.samples_high_gain[j, k] -= pedestal.mean_value_channel_high_gain[j, position_hg]
+            # for low gain
+            position_lg = int((k + event.first_capacitor_low_gain[j]) % size4drs)
+            event.samples_low_gain[j, k] -= pedestal.mean_value_channel_low_gain[j, position_lg]
--- release/readdragon.py
+++ release/readdragon.py
@@ -4,74 +4,62 @@ from protozfits.simple import File
 from pedestal import PedestalSimple, remove_pedestal
 from event import Event
 
-plt.rcParams.update({'font.size': 20})
-
-f = File("../data/Run021.1.fits.fz")
-N = 10000
-ped = PedestalSimple()
-
-for i in range(0, N):
-    ev = next(f.Events)
-    Ev = Event(ev)
-    Ev.read()
-    ped.fill_pedestal_event(Ev)
-
-ped.finalize_pedestal()
+plt.rcParams.update({'font.size': 18})
 
 
-f2 = File("../data/Run027.2.fits.fz")
-N2 = 2000
-event_hi_gain_before_remove_pedestal = np.zeros((N2, 8, 40))
-event_hi_gain_after_remove_pedestal = np.zeros((N2, 8, 40))
-for i in range(0, N2):
-    ev = next(f2.Events)
-    Ev = Event(ev)
-    Ev.read()
-    event_hi_gain_before_remove_pedestal[i, :, :] = Ev.samples_high_gain[:, :]
-    remove_pedestal(Ev, ped)
-    event_hi_gain_after_remove_pedestal[i, :, :] = Ev.samples_high_gain
+def plot_hist(event_data, gain):
+    position = [(0,0), (0, 1), (0,2), (0,3),
+                (1,0), (1, 1), (1,2), (1,3)]
+    fig, ax = plt.subplots(2,4)
+    for i in range(0, 8):
+        ax[position[i]].hist(event_data[:, i, 2:38].ravel(), bins=50, facecolor='green', alpha=0.75)
+        ax[position[i]].set_xlabel("signal [counts]")
+        ax[position[i]].set_ylabel("number of events")
+        sigma = np.std(event_data[:, i, 2:38].ravel())
+        mu = np.mean(event_data[:, i, 2:38].ravel())
+        ax[position[i]].set_title("{} Channel {}".format(gain, i + 1))
+        textstr = '$\mu=%.2f$\n$\sigma=%.2f$' % (mu, sigma)
+        props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+        ax[position[i]].text(0.05, 0.95, textstr, transform=ax[position[i]].transAxes, fontsize=14, verticalalignment='top', bbox=props)
+    plt.subplots_adjust(top=0.925, bottom=0.08, left=0.080, right=0.95, hspace=0.305, wspace=0.440)
 
 
+def _get_pedestal(file1, N1):
+    f1 = File(file1)
+    ped = PedestalSimple()
+    for i in range(0, N1):
+        ev = next(f1.Events)
+        Ev = Event(ev)
+        Ev.read()
+        ped.fill_pedestal_event(Ev)
+
+    ped.finalize_pedestal()
+
+    return ped
 
 
-position = [(0,0), (0, 1), (0,2), (0,3),
-            (1,0), (1, 1), (1,2), (1,3)]
+def subtract_pedestal(file1, file2, N1=10000, N2=3000):
+    ped = _get_pedestal(file1, N1)
+    f2 = File(file2)
+    event_hi_gain_before_remove_pedestal = np.zeros((N2, 8, 40))
+    event_hi_gain_after_remove_pedestal = np.zeros((N2, 8, 40))
+    event_low_gain_before_remove_pedestal = np.zeros((N2, 8, 40))
+    event_low_gain_after_remove_pedestal = np.zeros((N2, 8, 40))
+    for i in range(0, N2):
+        ev = next(f2.Events)
+        Ev = Event(ev)
+        Ev.read()
+        event_hi_gain_before_remove_pedestal[i, :, :] = Ev.samples_high_gain
+        event_low_gain_before_remove_pedestal[i, :, :] = Ev.samples_low_gain
+        remove_pedestal(Ev, ped)
+        event_hi_gain_after_remove_pedestal[i, :, :] = Ev.samples_high_gain
+        event_low_gain_after_remove_pedestal[i, :, :] = Ev.samples_low_gain
 
-fig, ax = plt.subplots(2,4)
-for i in range(0, 8):
-    ax[position[i]].hist(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel(), bins=50, facecolor='green', alpha=0.75)
-    sigma = np.std(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel().flatten())
-    mu = np.mean(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel().flatten())
-    ax[position[i]].set_title("Hi Gain Channel {}".format((i + 1)))
-    textstr = '$\mu=%.2f$\n$\sigma=%.2f$' % (mu, sigma)
-    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-    ax[position[i]].text(0.05, 0.95, textstr, transform=ax[position[i]].transAxes, fontsize=14, verticalalignment='top', bbox=props)
+    plot_hist(event_hi_gain_before_remove_pedestal, "Hi Gain")
+    plot_hist(event_hi_gain_after_remove_pedestal, "Hi Gain")
+    plot_hist(event_low_gain_before_remove_pedestal, "Low Gain")
+    plot_hist(event_low_gain_after_remove_pedestal, "Low Gain")
 
-
-
-plt.figure()
-for i in range(0, 8):
-    plt.subplot(2, 4, i+1)
-    plt.hist(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel(), bins=50, facecolor='green', alpha=0.75)
-    plt.xlabel("signal [counts]")
-    plt.ylabel("number of events")
-    sigma = np.std(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel().flatten())
-    mu = np.mean(event_hi_gain_before_remove_pedestal[:, i, 2:38].ravel().flatten())
-    textstr = '$\mu=%.2f$\n$\sigma=%.2f$' % (mu, sigma)
-    plt.title("Hi Gain Channel {}".format((i + 1)))
-
-plt.legend()
-
-plt.figure()
-for i in range(0, 8):
-    plt.subplot(2, 4, i+1)
-    plt.hist(event_hi_gain_after_remove_pedestal[:, i, 2:38].ravel(), bins=50, facecolor='green', alpha=0.75)
-    plt.xlabel("signal [counts]")
-    plt.ylabel("number of events")
-    sigma = np.std(event_hi_gain_after_remove_pedestal[:, i, 2:38].ravel().flatten())
-    mu = np.mean(event_hi_gain_after_remove_pedestal[:, i, 2:38].ravel().flatten())
-    textstr = '$\mu=%.2f$\n$\sigma=%.2f$' % (mu, sigma)
-    plt.title("Hi Gain Channel {}".format((i + 1)))
-plt.legend()
-
+subtract_pedestal("../data/Run021.1.fits.fz", "../data/Run027.2.fits.fz")
 plt.show()
+
